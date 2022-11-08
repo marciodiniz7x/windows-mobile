537de962677c4331aceef830da396fa22622118c
Márcio Diniz <marcio.sccp10@gmail.com>
2022-11-08 20:17:49 -0300

    Att

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Seletor de Elementos
 const telaBlock = document.querySelector('.telaBlock');
+const cadeado = document.querySelector('.cadeado');
 
 ///////////////////
 
 // ------------------- //
 
 // Script
+
+cadeado.addEventListener('click', () => {
+    telaBlock.classList.add('sumir');
+})
