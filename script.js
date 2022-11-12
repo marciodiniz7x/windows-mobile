7ac7c5aa75ea8f3a3b754c704c82d0b39797f0c3
Márcio Diniz <marcio.sccp10@gmail.com>
2022-11-11 22:14:44 -0300

    Atts

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 // Seletor de Elementos
 const telaBlock = document.querySelector('.telaBlock');
-const cadeado = document.querySelector('.cadeado');
+const telaInicial = document.querySelector('.telaInicial');
 
+const pesquisar = document.querySelector('.pesquisar');
+const digitar = document.querySelector('.digitar');
+
+const cadeado = document.querySelector('.cadeado');
 const btnPower = document.querySelector('.btnPower');
 
-///////////////////
 
-// ------------------- //
+// -------------------- //
+//////// Script /////////
 
-// Script
+let telaBloqueada = true;
+let telaDeBloqueio = false;
+let telaInicialLigada = false;
+btnPower.addEventListener('click', () => {
+    if (telaBloqueada === true) {
+        telaBlock.classList.remove('sumir');
+        telaBloqueada = false;
+        
+
+    } else if (telaBloqueada === false || telaInicialLigada == true) {
+        telaBlock.classList.add('sumir');
+        telaInicial.classList.add('sumir');
+        telaBloqueada = true;
+    }
+})
 
 cadeado.addEventListener('click', () => {
+    telaDeBloqueio = false;
     telaBlock.classList.add('sumir');
+    telaInicialLigada = true;
+    telaInicial.classList.remove('sumir');
+
 })
 
-btnPower.addEventListener('click', () => {
-    telaBlock.classList.toggle('sumir');
+digitar.addEventListener('focusin', () => {
+    pesquisar.classList.add('sumir')
 })
+
