e13f581b606603e9e61c943b2985558513f6ab90
Márcio Diniz <marcio.sccp10@gmail.com>
2022-11-19 18:27:26 -0300

    Atts

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const atualizaHora = () => {
     const pegarDia = data.getDay();
     const pegarHora = data.getHours();
 
-    const semana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+    const semana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'SÁBADO'];
 
     diaSemana.innerHTML = semana[pegarDia];
 }
